d1c9a2784eceebf9283ab0682607f29d0935e407
María Del Carmen Martín Rodríguez <mainen1985@gmail.com>
2025-03-20 21:24:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def update_output_container(selected_statistics, input_year):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run_server(debug=True)
 
 	
 
